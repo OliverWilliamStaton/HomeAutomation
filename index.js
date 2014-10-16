342d110b01c81af63ea85fa455716ba276c315a6
oliverstaton <oliver.william.staton@gmail.com>
2014-10-15 22:07:16 -0400

    Enabled ON/OFF control of GPIO pin-16

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var gpio = require('pi-gpio');
+
+// Write is working!!!
+//gpio.open(16, "output", function(err) {
+//	gpio.setDirection(16, "output", function(err) {
+//		gpio.write(16, 0, function(err) {
+//			if(err) throw err;
+//		});
+//	});
+//});
+
+// Read is working!!!
+//gpio.open(16, "output", function(err) {
+//	gpio.read(16, function(err, value) {
+//		if(err) throw err;
+//		console.log(value);
+//		gpio.close(16);
+//	});
+//});
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		console.log('message: ' + msg);
+	socket.on('turn on control', function(msg){
+		io.emit('turn on control', msg);
+		console.log('Control has been turned ' + msg);
+		
+		//gpio.open(16, "output", function(err) {
+			gpio.setDirection(16, "output", function(err) {
+				gpio.write(16, 1, function(err) {
+					if(err) throw err;
+				});
+			});
+		//});
+	});
+	
+	socket.on('turn off control', function(msg){
+		io.emit('turn off control', msg);
+		console.log('Control has been turned ' + msg);
+		
+		//gpio.open(16, "output", function(err) {
+			gpio.setDirection(16, "output", function(err) {
+				gpio.write(16, 0, function(err) {
+					if(err) throw err;
+				});
+			});
+		//});
 	});
 
 	console.log('a user connected');
